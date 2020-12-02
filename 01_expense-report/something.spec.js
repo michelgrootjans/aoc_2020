9e76bac80304b37eb0c1b61e82e673793d0e3d1a
Thomas Coopman <thomas.coopman@gmail.com>
2020-12-02 20:52:29 +0100

    start implementing findComplement
    commit 239e700efe9add210075271245fb4f882cf6a353
    Author: Michel Grootjans <michel.grootjans@persgroep.net>
    Date:   Wed Dec 2 19:50:50 2020 +0000
    
        mob next [ci-skip]
    
    commit a91ce669844839405298480f4fb9ccd3791d2399
    Author: Jeroen Tiebout <jeroen@mailbox.org>
    Date:   Wed Dec 2 20:45:08 2020 +0100
    
        mob next [ci-skip]

--- 01_expense-report/something.spec.js
+++ 01_expense-report/something.spec.js
@@ -1,5 +1,9 @@
+function findComplement(number, numbers) {
+  return 2019;
+}
+
 function expenseReport(numbers) {
-  return numbers[0] * numbers[1];
+  return numbers.reduce((result, input) => result * input)
 }
 
 describe('something', () => {
@@ -11,7 +15,17 @@ describe('something', () => {
     expect(expenseReport([2, 2018])).toBe(2 * 2018)
   });
 
+  xit('easy case 3 numbers, one 0', function () {
+    expect(expenseReport([0, 2, 2018])).toBe(2 * 2018)
+  });
+
   // it('the example', function () {
   //   expect(expenseReport([1721, 979, 366, 299, 675, 1456])).toBe(1721*299)
   // });
 });
+
+describe('oh yeah', () => {
+  it('should ', function () {
+    expect(findComplement(1, [9872345, 345, 2019, 2020, 98374])).toBe(2019)
+  });
+})
